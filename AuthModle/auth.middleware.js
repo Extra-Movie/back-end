63e813b80094738f09bf7b0700cd6ca421e0fba5
Eslam-Elwey <eslam.elwey@gmail.com>
2025-05-02 22:24:29 +0300

    Auth Routing And Middlewares

---
+++ AuthModle/auth.middleware.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+require("dotenv").config({ path: "../../.env" });
+
+module.exports = (req, res, next) => {
+  const token = req.header("Authorization")?.split(" ")[1];
+  if (!token)
+    return res
+      .status(401)
+      .json({ message: "Access denied. No token provided." });
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  if (!decoded) return res.status(401).json({ message: "Invalid token." });
+  req.userId = decoded.id;
+  req.isAdmin = decoded.isAdmin;
+  next();
+};
