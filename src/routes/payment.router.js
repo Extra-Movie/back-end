5490ee440ecefebf7e514662598549861d7a44a5
kareem172 <kareemkhalaf1722@gmail.com>
2025-05-08 10:51:34 +0300

    Refactor payment router by removing unused multer middleware import

--- src/routes/payment.router.js
+++ src/routes/payment.router.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const upload = require("../middlewares/multer.middleware");
-
 const authMiddleware = require("../middlewares/auth.middleware");
-const { createPaymentIntent, addToCart } = require("../controllers/payment.controller");
+const { createPaymentIntent } = require("../controllers/payment.controller");
 
-router.post("/create-payment-intent",authMiddleware, createPaymentIntent);
+router.post("/create-payment-intent", authMiddleware, createPaymentIntent);
 module.exports = router;
