eef70e7bea1ffaeeb4f39dc2a84222974c151091
ghadaahmed <ghadaahmed2772002@gmail.com>
2025-05-08 21:47:21 +0300

    all add,get for watchlist ,cart ,owned

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -6,14 +6,35 @@ const {
   updateProfile,
   deleteUser,
   makeAdmin,
+  addToWatchlist,
+  getWatchlist,
+  getCardsItems,
+  addToCart,
+  getOwned,
+  addToOwned
 } = require("../controllers/user.controller");
 const authMiddleware = require("../middlewares/auth.middleware");
 const adminMiddleware = require("../middlewares/admin.middleware");
+
+router.post("/addOwned", authMiddleware, addToOwned);
+router.get("/getOwned", authMiddleware, getOwned);
+
+
+
+router.get("/getCart", authMiddleware, getCardsItems);
+router.post("/addToCart", authMiddleware, addToCart);
+
+
+
+router.post("/watchlist", authMiddleware, addToWatchlist);
+router.get("/getWatchlist", authMiddleware, getWatchlist);
 
 router.get("/", authMiddleware, adminMiddleware, getAllUsersData);
 router.get("/:id", authMiddleware, getUserDataById);
 router.put("/:id", authMiddleware, updateProfile);
 router.delete("/:id", authMiddleware, deleteUser);
 router.patch("/:id", authMiddleware, adminMiddleware, makeAdmin);
+
+
 
 module.exports = router;
