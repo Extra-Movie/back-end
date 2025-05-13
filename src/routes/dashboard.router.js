eb89d14dee174e445a0f393e531d90b8fe202e38
OmarWaled8008 <omarwaled8888@gmail.com>
2025-05-13 19:21:18 +0300

    Add authentication and admin middleware to dashboard routes

--- src/routes/dashboard.router.js
+++ src/routes/dashboard.router.js
@@ -15,29 +15,61 @@ const {
   watchlistTrends,
   abandonedCarts,
 } = require("../controllers/dashboard.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
+const adminMiddleware = require("../middlewares/admin.middleware");
 
 //#region User Dashboard
-router.get("/total-users", totalUsers);
-router.get("/users-by-role", usersByRole);
-router.get("/user-growth", userGrowth);
+router.get("/total-users", authMiddleware, adminMiddleware, totalUsers);
+router.get("/users-by-role", authMiddleware, adminMiddleware, usersByRole);
+router.get("/user-growth", authMiddleware, adminMiddleware, userGrowth);
 // #endregion
 
 //#region Sales Dashboard
-router.get("/total-sales", totalSales);
-router.get("/total-revenue", totalRevenue);
-router.get("/monthly-sales-trend", monthlySalesTrend);
-router.get("/top-selling-content", topSellingContent);
+router.get("/total-sales", authMiddleware, adminMiddleware, totalSales);
+router.get("/total-revenue", authMiddleware, adminMiddleware, totalRevenue);
+router.get(
+  "/monthly-sales-trend",
+  authMiddleware,
+  adminMiddleware,
+  monthlySalesTrend
+);
+router.get(
+  "/top-selling-content",
+  authMiddleware,
+  adminMiddleware,
+  topSellingContent
+);
 // #endregion
 
 //#region Movie & TV Show Dashboard
-router.get("/most-visited-content", mostVisitedContent);
-router.get("/top-rated-content", topRatedContent);
-router.get("/content-by-genre", contentbyGenre);
+router.get(
+  "/most-visited-content",
+  authMiddleware,
+  adminMiddleware,
+  mostVisitedContent
+);
+router.get(
+  "/top-rated-content",
+  authMiddleware,
+  adminMiddleware,
+  topRatedContent
+);
+router.get(
+  "/content-by-genre",
+  authMiddleware,
+  adminMiddleware,
+  contentbyGenre
+);
 // #endregion
 
 //#region Cart & Watchlist Dashboard
-router.get("/cart-frequency", cartFrequency);
-router.get("/watchlist-trends", watchlistTrends);
+router.get("/cart-frequency", authMiddleware, adminMiddleware, cartFrequency);
+router.get(
+  "/watchlist-trends",
+  authMiddleware,
+  adminMiddleware,
+  watchlistTrends
+);
 //#endregion
 
 module.exports = router;
